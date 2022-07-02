f972bc93998fbc9debb3fd61cdd0dcbd0a787094
byronRC10 <byronredondo29@gmail.com>
2022-07-02 13:06:15 -0600

    Agregar archivos de proyecto.

---
+++ TestProjectX/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace TestProjectX
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PruebaString()
+        {
+            string esperado = "Cuarenta y dos";
+            string actual = "Cuarenta";
+
+            Assert.Equal(esperado,actual);
+        }
+
+        [Fact]
+        public void PruebaNumeros()
+        {
+            int esperado = 42;
+            int actual = 6 * 7;
+
+            Assert.Equal(esperado, actual);
+        }
+    }
+}
